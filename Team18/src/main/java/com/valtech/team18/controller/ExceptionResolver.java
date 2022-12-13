fffa61fd9a89e05733139cbcf04c51df17ebb18e
Anuj S M <Anuj.SM@valtech.com>
2022-12-13 18:30:34 +0530

    pending tables changes

---
+++ Team18/src/main/java/com/valtech/team18/controller/ExceptionResolver.java
@@ -0,0 +1,23 @@
+package com.valtech.team18.controller;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@RestControllerAdvice
+public class ExceptionResolver {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public HashMap<String, String> handleNoHandlerFound(NoHandlerFoundException e, WebRequest request) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "fail");
+        response.put("message", e.getLocalizedMessage());
+        return response;
+    }
+}
