1c56699402dabc4a88199812b855529ac157aad1
Anuj S M <Anuj.SM@valtech.com>
2022-12-14 15:08:00 +0530

    no changes

---
+++ Team18/src/main/java/com/valtech/team18/service/UserDetailsService.java
@@ -0,0 +1,13 @@
+//package com.valtech.team18.service;
+//
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//
+//public interface UserDetailsService {
+//
+//	/* (non-Javadoc)
+//	 * @see com.retail.delight.service.UserDetailsService#loadUserByUsername(java.lang.String)
+//	 */
+//	UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+//
+//}
